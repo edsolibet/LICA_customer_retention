ca74256730757423c319570292a81c695009ad7d
Erick John Carlo D. Solibet <carlo.solibet@gmail.com>
2022-08-02 18:27:19 +0800

    Add files via upload

--- mechanigo_customer_retention.py
+++ mechanigo_customer_retention.py
@@ -425,7 +425,7 @@ def gamma_gamma_model(df_cohort):
     return df_cohort, ggf
 
 def search_for_name(name, df_data):
-  df_data.full_name = df_data.apply(lambda x: x['full_name'].lower())
+  df_data.full_name = df_data.apply(lambda x: x['full_name'].lower(), axis=1)
   names = df_data[df_data.apply(lambda x: name.lower() in x['full_name'], axis=1)]
   df_temp = names[['customer_id','full_name', 'brand', 'model/year','fuel_type',
                    'transmission','plate_number','phone','address','mileage',
@@ -435,7 +435,7 @@ def search_for_name(name, df_data):
 
 def search_for_name_retention(name, df_cohort):
     df_cohort = df_cohort.reset_index()
-    df_cohort = df_cohort.apply(lambda x: x['full_name'].lower())
+    df_cohort = df_cohort.apply(lambda x: x['full_name'].lower(), axis=1)
     names_retention = df_cohort[df_cohort.apply(lambda x: name.lower() in x['full_name'], axis=1)]
     df_temp_retention = names_retention[['full_name', 'recency', 'frequency', 'T', 
                                        'total_sales', 'avg_sales', 'ITT', 'last_txn',
