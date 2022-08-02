5cb5d2f6c7f53fc1e0bfc0fd8ee0ea184cd3d29f
Erick John Carlo D. Solibet <carlo.solibet@gmail.com>
2022-08-02 09:27:15 +0800

    mechanigo_customer_retention.py
    Added bar plot of ITT and avg sales for single/multiple customers

--- mechanigo_customer_retention.py
+++ mechanigo_customer_retention.py
@@ -24,11 +24,35 @@ import numpy as np
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
-import statsmodels
+import statsmodels, re, string
 from datetime import datetime
 from joblib import dump, load
 from lifetimes.fitters.pareto_nbd_fitter import ParetoNBDFitter
+from lifetimes.plotting import plot_probability_alive_matrix
 from lifetimes import GammaGammaFitter
+
+def remove_emoji(text):
+    emoj = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642" 
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"
+                      "]+", re.UNICODE)
+    return re.sub(emoj, ' ', text).strip()
 
 def fix_name(name):
   '''
@@ -44,12 +68,20 @@ def fix_name(name):
     - fixed name; str
   '''
   name_list = list()
+  # removes emojis and ascii characters (i.e. chinese chars)
+  name = remove_emoji(name).encode('ascii', 'ignore').decode()
+  # split name by spaces
   for n in name.split(' '):
     if n not in name_list:
-      name_list.append(n)
+    # check each character for punctuations minus '.' and ','
+      name_list.append(''.join([ch for ch in n 
+                                if ch not in string.punctuation.replace('.', '')]))
+    else:
+        continue
   return ' '.join(name_list).strip()
 
 def get_ratio(a, b):
+    
   try:
     return a/b
   except:
@@ -167,15 +199,17 @@ def cohort_rfm(df):
                                        recency=('date', lambda x: (x.max() - x.min()).days),
                                        frequency=('id', lambda x: len(x) - 1),
                                        total_sales=('total_cost', lambda x: np.sum(x)),
+                                       avg_sales=('avg_cost', lambda x: np.mean(x)),
                                        T = ('date', lambda x: (datetime.today()-x.min()).days + 1),
                                        year=('date', lambda x: x.min().year),
                                        month=('date', lambda x: x.min().month),
                                        )
-    df_cohort.columns = ['cohort', 'recency', 'frequency', 'total_sales', 'T', 'year', 'month']
-    df_cohort.loc[:,'avg_days_between_purchase'] = df_cohort.apply(lambda row: get_ratio(row['recency'], row['frequency']), axis=1)
+    df_cohort.columns = ['cohort', 'recency', 'frequency', 'total_sales', 
+                         'avg_sales', 'T', 'year', 'month']
+    df_cohort.loc[:,'ITT'] = df_cohort.apply(lambda row: get_ratio(row['recency'], row['frequency']), axis=1)
     df_cohort = df_cohort.fillna(0)
     # filter data by returning customers
-    df_cohort = df_cohort[df_cohort['total_sales'] > 0]
+    df_cohort = df_cohort[df_cohort['avg_sales'] > 0]
     
     return df_cohort
 
@@ -219,32 +253,70 @@ def customer_lv(df_cohort):
     ax1[0].set_ylim([0, round(customer_lv.clv.max()*1.2)])
     ax1[0].set_ylabel('customer clv', color=colors[0][0])
     ax1[0].axhline(y=customer_lv_.clv.mean(), color='black', linestyle='--');
+    plt.title('Cohort Lifetime Value', fontsize=14);
     # set secondary y-axis
     ax2 = ax1[0].twinx()
     ax2.plot(range(len(customer_lv_.cohort)), customer_lv_.churn, '--o', color=colors[0][1])
     ax2.set_ylim([0.5, 1])
-    ax2.set_ylabel('churn %', color=colors[0][1])
+    ax2.set_ylabel('churn %', color=colors[0][1], fontsize=12)
     # set shared x-label and x-ticks
     ax2.set_xticks(range(len(customer_lv_.cohort)))
     ax2.set_xticklabels(customer_lv_.cohort)
-    ax2.set_xlabel('first_cohort');
+    ax2.set_xlabel('first_cohort', fontsize=12);
     
     ax1[1].plot(range(len(customer_lv_.cohort)), customer_lv_.avg_sales, '--o', color=colors[1][0]);
     #ax1[1].set_ylim([0, round(customer_clv.clv.max()*1.2)])
-    ax1[1].set_ylabel('avg sales', color=colors[1][0])
+    ax1[1].set_ylabel('avg sales', color=colors[1][0], fontsize=12)
     # set secondary y-axis
     ax2 = ax1[1].twinx()
     ax2.plot(range(len(customer_lv_.cohort)), customer_lv_.purchase_freq, '--o', color=colors[1][1])
     #ax2.set_ylim([0.5, 1])
-    ax2.set_ylabel('purchase freq', color=colors[1][1])
+    ax2.set_ylabel('purchase freq', color=colors[1][1], fontsize=12)
     # set shared x-label and x-ticks
     ax2.set_xticks(range(len(customer_lv_.cohort)))
     ax2.set_xticklabels(customer_lv_.cohort)
     fig.autofmt_xdate(rotation=90, ha='right')
-    ax2.set_xlabel('first_cohort');
+    ax2.set_xlabel('first_cohort', fontsize=12);
     
     st.pyplot(fig)
-    return df_cohort, customer_lv
+    return customer_lv
+
+def bar_plot(df_cohort):
+    '''
+    Plots inter-transaction time of returning customers
+
+    Parameters
+    ----------
+    df_cohort : dataframe
+
+    Returns
+    -------
+    ITT plot
+
+    '''
+    option = st.selectbox('Variable to show: ', 
+                          ('Inter-transaction time', 'Average Sales'))
+    choice = {'Inter-transaction time': 'ITT',
+              'Average Sales': 'avg_sales'}
+    fig, ax1 = plt.subplots()
+    bins = st.slider('Bins: ', 5, 40, 8)
+    a = df_cohort[df_cohort['frequency'] == 1][choice[option]]
+    b = df_cohort[df_cohort['frequency'] > 1][choice[option]]
+    ax1.hist([a.values, b.values], bins=bins, label=['Single', 'Multiple'])
+    plt.xlabel('Days', fontsize=12)
+    plt.ylabel('Number of customers', fontsize=12)
+    plt.title('Inter-transaction time by returning customers', fontsize=14);
+    plt.legend()
+    plt.tight_layout()
+    st.pyplot(fig)
+
+def pareto_NBD_model(df_cohort, t):
+    pnbd = ParetoNBDFitter(penalizer_coef=0.001)
+    pnbd.fit(df_cohort['frequency'], df_cohort['recency'], df_cohort['T'])
+    # calculate probability of active
+    df_cohort['prob_alive'] = df_cohort.apply(lambda x: pnbd.conditional_probability_alive(t, x['frequency'], x['recency'], x['T']), 1)
+    
+    return df_cohort, pnbd
 
 if __name__ == '__main__':
     # import data and preparation
@@ -266,3 +338,9 @@ if __name__ == '__main__':
              of its components (frequency, average total sales, churn%).
              ''')
     clv = customer_lv(df_cohort)
+    # plots ITT
+    st.write('''
+             This bar plot shows the distribution of single/multiple repeat 
+             transaction(s) based on:
+             ''')
+    bar_plot(df_cohort)
