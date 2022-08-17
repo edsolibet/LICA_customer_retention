cd7163ae2d6a505ba23163a9dfe971a6d996d2f4
Erick John Carlo D. Solibet <carlo.solibet@gmail.com>
2022-08-17 23:37:44 +0800

    Update mechanigo_customer_retention.py

--- mechanigo_customer_retention.py
+++ mechanigo_customer_retention.py
@@ -331,10 +331,10 @@ def customer_lv_(df_retention):
     for i, col in enumerate(data, start=1):
         fig.add_trace(go.Scatter(x=cohorts, y=data[i-1],
                                  line = dict(width=4, dash='dash')),
-                         row=i, col=1)
+                         row=i, col=1, name=y_labels[i-1])
         fig.update_yaxes(title_text = y_labels[i-1], row=i, col=1)
 
-    fig.update_layout(title_text = 'Cohort CLV characteristics'
+    fig.update_layout(title_text = 'Cohort CLV characteristics',
                      height = 800,
                      width = 600)
     st.plotly_chart(fig)
